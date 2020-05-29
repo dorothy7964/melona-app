3c1454802dbbb8133b649a1e0657c3a17abcc368
sample <dorothy_007@naver.com>
2020-05-29 20:05:56 +0900

    #01.03 isLoggedIn part

---
+++ styles.js
@@ -0,0 +1,10 @@
+export default {
+    blackColor: "#262626",
+    greyColor: "#FAFAFA",
+    darkGreyColor: "#999",
+    lightGreyColor: "#eee",
+    darkGreyColor: "#999",
+    redColor: "#ED4956",
+    melonaColor: "#b9dd39",
+    lightGreenColor: "#9ccc65"
+};
