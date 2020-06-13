d3d7a64d7a65af18b0fa5d83d4acb04bf4595b95
sample <dorothy_007@naver.com>
2020-06-14 04:31:52 +0900

    #04.02 navigation - ConfirmNavigation

---
+++ navigation/ConfirmNavigation.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Buy from "../screens/Confirm/Buy";
+import Apply from "../screens/Confirm/Apply";
+
+const ConfirmNavigation = createStackNavigator();
+
+export default () => {
+    return (
+        <ConfirmNavigation.Navigator initialRouteName="Buy" headerMode="none">
+            <ConfirmNavigation.Screen name="Buy" component={Buy} />
+            <ConfirmNavigation.Screen name="Apply" component={Apply} />
+        </ConfirmNavigation.Navigator>
+    );
+};
