7a91c57e8b6514388d94bf874ea969322220943a
sample <dorothy_007@naver.com>
2020-06-14 23:06:21 +0900

    #05.02 TabsNavigation - NavigationOptions Tab Icons, label

---
+++ components/NavText.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import styles from "../styles";
+
+const Text = styled.Text`
+    color: ${props => props.color};
+    font-size: 12;
+`;
+
+const NavText = ({
+    focused = true,
+    text,
+    color = styles.melonaColor,
+}) => (
+    <Text color={`${focused ? color : styles.darkGreyColor}`}>
+        {text}
+    </Text>
+);
+NavText.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    focused: PropTypes.bool
+};
+
+export default NavText;
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler';
 import * as React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Daddy from "../screens/Tabs/Daddy";
 import Daughter from "../screens/Tabs/Daughter";
 import Group from "../screens/Tabs/Group";
 import Message from "../screens/Tabs/Message";
 import Profile from "../screens/Tabs/Profile";
+import NavText from "../components/NavText";
+import NavIcon from "../components/NavIcon";
 import StackFactory from "./StackFactory";
 
 const TabNavigation = createBottomTabNavigator();
@@ -15,7 +18,24 @@ export default () => {
         <TabNavigation.Navigator>
             <TabNavigation.Screen 
                 name="갈 때 사갈게" 
-                component={StackFactory} 
+                component={StackFactory}   
+                options={{
+                    tabBarLabel: ({ focused }) => (
+                        <NavText 
+                            text="갈 때 사갈게"
+                            focused={focused}
+                        />
+                    ),
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={ Platform.OS === "ios"
+                                ? "ios-checkbox-outline" 
+                                : "md-checkbox-outline"
+                            }
+                        />
+                    )
+                }} 
                 initialParams={{
                     initialRoute: Daddy,
                     customConfig: {
@@ -28,7 +48,24 @@ export default () => {
             />
             <TabNavigation.Screen 
                 name="올 때 사다줘" 
-                component={StackFactory} 
+                component={StackFactory}  
+                options={{
+                    tabBarLabel: ({ focused }) => (
+                        <NavText 
+                            text="올 때 사다줘"
+                            focused={focused}
+                        />
+                    ),
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={Platform.OS === "ios" 
+                                ? "ios-checkbox-outline" 
+                                : "md-checkbox-outline"
+                            }
+                        />
+                    )
+                }} 
                 initialParams={{
                     initialRoute: Daughter,
                     customConfig: {
@@ -40,8 +77,25 @@ export default () => {
                 }}
             />
             <TabNavigation.Screen
-                name="그룹" 
+                name="그룹"
                 component={StackFactory} 
+                options={{
+                    tabBarLabel: ({ focused }) => (
+                        <NavText 
+                            text="그룹"
+                            focused={focused}
+                        />
+                    ),
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={Platform.OS === "ios" 
+                                ? "ios-people" 
+                                : "md-people"
+                            }
+                        />
+                    )
+                }}
                 initialParams={{
                     initialRoute: Group,
                     customConfig: {
@@ -54,7 +108,24 @@ export default () => {
             />
             <TabNavigation.Screen
                 name="메시지" 
-                component={StackFactory} 
+                component={StackFactory}  
+                options={{
+                    tabBarLabel: ({ focused }) => (
+                        <NavText 
+                            text="메시지"
+                            focused={focused}
+                        />
+                    ),
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={Platform.OS === "ios" 
+                                ? "ios-text" 
+                                : "md-text"
+                            }
+                        />
+                    )
+                }}
                 initialParams={{
                     initialRoute: Message,
                     customConfig: {
@@ -67,7 +138,24 @@ export default () => {
             />
             <TabNavigation.Screen
                 name="프로필" 
-                component={StackFactory} 
+                component={StackFactory}  
+                options={{
+                    tabBarLabel: ({ focused }) => (
+                        <NavText 
+                            text="프로필"
+                            focused={focused}
+                        />
+                    ),
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={Platform.OS === "ios" 
+                                ? "ios-person" 
+                                : "md-person"
+                            }
+                        />
+                    )
+                }}
                 initialParams={{
                     initialRoute: Profile,
                     customConfig: {
